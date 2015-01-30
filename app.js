830ce20fb6503e3156195d78b1d883f9e209b975
Sambillingham <sam.billingham@steer.io>
2015-01-30 16:43:24 +0000

    pseudo code for next step

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ var currentPage = horseman
   	.waitForNextPage()
   	.click('a[href="/enterprise/BookingsCentre/MemberTimetable"]')
   	.waitForNextPage()
+  	// .click( $( "div:contains('&nbsp;&nbsp;Monday - 02 February 2015')" ).parent().parent().next().next().find("a:contains('Warrior Workout')"").parent().parent().parent().find('#button') )
+  	// .waitForNextPage()
+  	// .title // try getting the title of confirmation page
+  	// .click() find confirm button
+  	//  .waitForNextPage()
   	.title()
 
 console.log("On page: " + currentPage);
