6dc05178bf462b8a47ee4b55add15989afedea7c
Sambillingham <sam.billingham@steer.io>
2015-03-23 16:00:24 +0000

    dom traversal

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
 var Horseman = require('node-horseman'),
 	  horseman = new Horseman(),
 	  config = require('./config'),
-    schedule = require('node-schedule');
+    schedule = require('node-schedule'),
+    util = require('util');
 
-function bookOnGymbox(booking, day) {
+var frame = 'Krav Maga';
+var wed = '24 March'
+
+function bookOnGymbox(className, date) {
   var currentPage = horseman
     	.open('https://gymbox.legendonlineservices.co.uk/enterprise/account/Login')
     	.type('#login_Email', config.email)
-    	.type('#login_password', config.password)
+    	.type('#login_Password', config.password)
     	.click('#login')
     	.waitForNextPage()
     	.click('a[href="/enterprise/BookingsCentre/MemberTimetable"]')
     	.waitForNextPage()
-    	// .click( $( "div:contains('&nbsp;&nbsp;Monday - 02 February 2015')" ).parent().parent().next().next().find("a:contains('Warrior Workout')"").parent().parent().parent().find('#button') )
+      .manipulate( function(date, className) {
+        $(":contains("+date+")").parent().parent().nextAll().find("a:contains("+className+")").eq(0).parent().parent().parent().find("a:contains('Book')").addClass('class-to-book');
+        $(":contains("+date+")").parent().parent().nextAll().find("a:contains("+className+")").eq(0).parent().parent().parent().find("a:contains('Book')").css('background', 'red');
+      }, date, className )
+      .click('.class-to-book')
+      .waitForNextPage()
+      .click('#btnPayNow')
+      .waitForNextPage()
+      .screenshot('screenshots/complete.png')    
+    	// .click( )
     	// .waitForNextPage()
-    	// .title // try getting the title of confirmation page
-    	// .click() find confirm button
-    	//  .waitForNextPage()
-    	.title()
-
+    	// .html('.formBand') // try getting the title of confirmation page
+    	// // // .click() find confirm button
+    	// // //  .waitForNextPage()
+    	// .html('.formBand')
+// 
   console.log("On page: " + currentPage);
+
+  // console.log(util.inspect(currentPage, {showHidden: false, depth: 2}));
 
   horseman.close();
 }
+
+function checkBasket(){
+  var basket = horseman
+      .open('https://gymbox.legendonlineservices.co.uk/enterprise/account/Login')
+      .type('#login_Email', config.email)
+      .type('#login_Password', config.password)
+      .click('#login')
+      .waitForNextPage()
+      .click('a[href="/enterprise/Basket/Index"]')
+      .waitForNextPage()
+      .screenshot('screenshots/basket.png')   
+  
+  horseman.close();
+}
+
+bookOnGymbox(frame, wed);
+// checkBasket();
 
 function createBooking(){
 
@@ -32,3 +64,5 @@ function createBooking(){
 
   return j;
 }
+
+//$("h5:contains('Wednesday - 25 March 201')").parent().parent().nextAll().find("a:contains('Frame Fitness')").eq(0).parent().parent().parent().find("a:contains('Book')").css('background', 'green')
